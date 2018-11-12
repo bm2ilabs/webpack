05ccaa67a69b248400147abb600353c528369d8a
Boukraa Mohamed <bm2ilabs@gmail.com>
2018-11-12 14:10:16 +0100

    Add Sass compiler

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,20 @@ module.exports = config => (env, argv) =>
                             },
                         ],
                     },
+                    {
+                        test: /\.scss$/,
+                        use: [
+                            {
+                                loader: "style-loader" // creates style nodes from JS strings
+                            },
+                            {
+                                loader: "css-loader" // translates CSS into CommonJS
+                            },
+                            {
+                                loader: "sass-loader" // compiles Sass to CSS
+                            }
+                        ]
+                    }
                 ],
             },
 
